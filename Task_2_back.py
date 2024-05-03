8b3777474dbeeeb961ed8fa80cb00ab8486a723f
M-Jalakas <127521234+M-Jalakas@users.noreply.github.com>
2024-05-03 15:05:36 +0200

    Uppdatering

--- Task_2_back.py
+++ Task_2_back.py
@@ -1,19 +1,19 @@
-
 ###########################################################################
 ############### IMPORTS AND DATA #########################################
 ###########################################################################
 
 # Imports
-import warnings # removes annoying error message
-warnings.simplefilter(action='ignore', category=FutureWarning) # removes annoying error message
-import pandas as pd # Matte
-import numpy as np # Matte
-import matplotlib as plt # Visualisering
-import matplotlib.pyplot as plt # Visualisering
+import warnings  # removes annoying error message
+
+warnings.simplefilter(action='ignore', category=FutureWarning)  # removes annoying error message
+import pandas as pd  # Matte
+import numpy as np  # Matte
+import matplotlib as plt  # Visualisering
+import matplotlib.pyplot as plt  # Visualisering
 
 # path to data from directory
-train = pd.read_csv('train-final.csv',encoding='latin-1',sep=',')
-test = pd.read_csv('test-final.csv',encoding='latin-1',sep=',')
+train = pd.read_csv('train-final.csv', encoding='latin-1', sep=',')
+test = pd.read_csv('test-final.csv', encoding='latin-1', sep=',')
 
 ###########################################################################
 ############### GER DATAFRAMSEN HEADER MED VAD KOLUMNERNA INNEHÅLLER#######
@@ -47,11 +47,11 @@ train_head = pd.DataFrame(reshaped_train, columns=names)
 ###########################################################################
 
 # here the data is prepared for testing and training
- # hides the 'word'- and 'index'-columns
+# hides the 'word'- and 'index'-columns
 train_blind = train_head.drop(['word', 'index'], axis=1)
 test_blind = test_head.drop(['word', 'index'], axis=1)
 
- # makes everything into the same datatype
+# makes everything into the same datatype
 test_blind = pd.DataFrame(data=test_blind, dtype=np.float64)
 train_blind = pd.DataFrame(data=train_blind, dtype=np.float64)
 
@@ -62,59 +62,77 @@ train_blind = pd.DataFrame(data=train_blind, dtype=np.float64)
 # stores both dataframes in a dictionary so that they can be accessed in a loop
 all_data = [train_head, test_head]
 
-for df_index, df in enumerate(all_data):
-    # Check if DataFrames contain NaN elements
-    if df.isna().any().any():
+for df in range(len(all_data)):
+
+    # checks if dataframes has NaN-elements
+    if all_data[df].isna().any().any():
         print("DataFrame contains NaN elements")
     else:
         print("DataFrame does not contain NaN elements")
 
-    # Find rows with NaN values anywhere in the DataFrame
-    rows_with_nan = df[df.isna().any(axis=1)]
+    # find rows with NaN values anywhere in the DataFrame
+    rows_with_nan = all_data[df][all_data[df].isna().any(axis=1)]
 
-    # Display the words of 'word' column for rows with NaN values
+    # display the words of 'word' column for rows with NaN values
+    words_with_nan = rows_with_nan[['word', 'index']]
+    # print(words_with_nan)
+
+    # then this to be able to select and save an index
     for idx, row in rows_with_nan.iterrows():
-        print(f"Word: {row['word']}, Index: {row['index']}")
+        # What word was picked:
+        nan_word = row['index']  # This gets the 'word' from the current row
+        # print(f"Word: {nan_word}, Index: {row['index']}")
 
-        # Find all instances where 'index' matches
-        gest_all = df[df['index'] == row['index']]
+        # here all instances of the word are stored in gest_all
+        gest_all = all_data[df][all_data[df]['index'] == nan_word]
+        # print(gest_all)
 
-        # Check if there are any NaN values in the selected rows
-        if gest_all.iloc[:, 0:180].isna().any().any():
-            print("gest_all contains NaN elements")
-        else:
-            print("gest_all does not contain NaN elements")
+        # check if there are any NaN values in the DataFrame for frame 1 of 'word'
+        # if gest_all[0:60].isna().any().any():
+        # print("DataFrame contains NaN elements")
+        # else:
+        # print("DataFrame does not contain NaN elements")
 
-        # Dictionary to hold values by coordinate and index
-        x_values, y_values, z_values = {}, {}, {}
+        # creates dictionaries to hold values for each index
+        x_values = {}
+        y_values = {}
+        z_values = {}
 
-        # Process each column in gest_all
-        for column in gest_all.columns[:180]:  # Assuming first 180 columns are relevant
-            if '_' in column:
-                coord_type = column.split('_')[0]
-                index = int(column.split('_')[1])
+        # loops through each instance
+        for i in range(len(gest_all)):
+            gest = gest_all.iloc[i]
+            gest_xyz = gest.iloc[0:120]  # <-- this is the frame we choose
 
-                # Store values in the corresponding dictionary
-                if coord_type == 'x':
-                    x_values.setdefault(index, []).append(gest_all[column].dropna())
-                elif coord_type == 'y':
-                    y_values.setdefault(index, []).append(gest_all[column].dropna())
-                elif coord_type == 'z':
-                    z_values.setdefault(index, []).append(gest_all[column].dropna())
+            # extracts index from column names
+            for column in gest_xyz.index:
+                index = int(column.split('_')[1])  # chooses all column-indexes in interval
 
-    # Replace all NaN in the main dataframe for each coordinate type
+                # separates values by index and stores them in the corresponding dictionary
+                if column.startswith('x'):
+                    x_values.setdefault(index, []).append(gest_xyz[column])
+                elif column.startswith('y'):
+                    y_values.setdefault(index, []).append(gest_xyz[column])
+                elif column.startswith('z'):
+                    z_values.setdefault(index, []).append(gest_xyz[column])
+
+    # now finally replace all NaN in the main dataframe
+    # iterate over each dictionary
     for prefix, dictionary in [('x', x_values), ('y', y_values), ('z', z_values)]:
+        # iterate over each column index
         for index, values in dictionary.items():
+            # get the column name
             column_name = f'{prefix}_{index}'
-            if column_name in df.columns:
-                # Calculate mean of non-NaN values
-                non_nan_values = pd.concat(values)  # Concatenate all series for this index
-                mean_value = non_nan_values.mean()
-                # Replace NaN values with the mean
-                df[column_name].fillna(mean_value, inplace=True)
+            # replace NaN values with the mean of non-NaN values for the corresponding column in main dataframe
+            all_data[df][column_name].fillna(np.nanmean(values), inplace=True)
+            # for element in main dataframe all NaN are now replaced with the mean of respective list
 
+    # checks again if the modified dataframe train_head contains any NaN
+    if all_data[df].isna().any().any():
+        print("DataFrame still contains NaN elements")
+    else:
+        print("DataFrame does not contain NaN elements anymore")
 
-
+#########################################
 '''
 ###########################################################################
 ############################## data prepp #################################
@@ -177,15 +195,6 @@ for index in x_values.keys():
   print(f"z_{index}: {z_values[index]}")
   print()
 
-#
-
-#################### knn #######################################
-#TA REDA PÅ k
-#har för mig att det finns en regel att knn vill ha sqrt(datapoints) som k
-    #antingen om vi utgår från en algoritm som hittar minsta 'word'-arrayen och tar roten..
-    #..ur den, det är illafall en början
-    #OM det inte fungerar, så kan vi ta sqrt(word_array)=k för varje ord, men blir...
-    #... betydligt mindre optimerat, vilket knn redan är för stora mängder data
 #############################################################
 
 # + alla andra classifiers
@@ -247,13 +256,22 @@ from sklearn.neighbors import KNeighborsClassifier
 # training data
 train = train_head.iloc[:, 0:60]
 
-train_label = train_head['index']
-#train_label = train_label.astype(str)
+train_label = train_head.iloc[:, 241:242]
+train_label = train_label.astype(str)
+train_label = np.array(train_label)
+train_label = np.ravel(train_label)
+
+train = train * 10 ** 10
+train = train.astype(int)
 
 # test data
 test = test_head.iloc[:, 0:60]
-
-test_label = test_head['index']
+test_label = test_head.iloc[:, 241:242]
+test_label = test_label.astype(str)
+test_label = np.array(test_label)
+test_label = np.ravel(test_label)
+test = test * 10 ** 10
+test = test.astype(int)
 
 # check if there are any NaN values in gest_all
 if test.isna().any().any():
@@ -261,10 +279,50 @@ if test.isna().any().any():
 else:
     print("knn-set does not contain NaN elements")
 
-
 # Train a KNN model with default model hyperparameters
-knn = KNeighborsClassifier(n_neighbors=5)
+knn = KNeighborsClassifier(n_neighbors=20)
 knn.fit(train, train_label)
-    
+
 # Make predictions on the testing set
-y_pred = knn.predict(test)
+pred = knn.predict(test)
+print("svar", pred)
+
+print("we want the square of datapoints:", (len(test_label)) ** (1 / 2))
+
+# use twice the square of the datapoints to look for the optimized k-value
+
+######### OPTIMIZING ERROR MARIGN #################
+from sklearn.metrics import mean_squared_error
+
+# Try different values of k neighbors
+ks = [x for x in range(1, 400) if x % 2 != 0]  # choose a range with uneven integers
+errors = []
+
+# a loop that tests all k values and outputs the error
+for k in ks:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(train, train_label)
+    y_pred = knn.predict(test)
+    error = mean_squared_error(test_label, y_pred)
+    errors.append(error)
+
+# Choose the optimal k (minimal error)
+optimal_k = ks[np.argmin(errors)]
+print(f'Optimal k: {optimal_k}')
+
+# Plot the error curve
+import matplotlib.pyplot as plt
+
+plt.plot(ks, errors)
+plt.xlabel('k')
+plt.ylabel('Error')
+plt.title('Error Curve for knn Algorithm')
+plt.show()
+
+#################### knn #######################################
+# TA REDA PÅ k
+# har för mig att det finns en regel att knn vill ha sqrt(datapoints) som k
+# antingen om vi utgår från en algoritm som hittar minsta 'word'-arrayen och tar roten..
+# ..ur den, det är illafall en början
+# OM det inte fungerar, så kan vi ta sqrt(word_array)=k för varje ord, men blir...
+# ... betydligt mindre optimerat, vilket knn redan är för stora mängder data
