0c763de43f076169ada536852148fd21c8f80aaf
M-Jalakas <127521234+M-Jalakas@users.noreply.github.com>
2024-05-17 14:38:53 +0200

    treeeeeeeee

---
+++ decisionTree.py
@@ -0,0 +1,14 @@
+# Data Processing
+import pandas as pd
+import numpy as np
+
+# Modelling
+from sklearn import tree
+
+# Data
+from main import test_processed
+from main import train_processed
+
+
+
+
